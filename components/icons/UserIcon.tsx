2c6e821335b927bae92f459dce57761f28cdb480
Jason Maxwell <max371979@gmail.com>
2025-11-06 16:16:28 -0500

    Create UserIcon.tsx

---
+++ components/icons/UserIcon.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const UserIcon: React.FC = () => (
+    <div className="w-10 h-10 bg-gray-300 dark:bg-gray-600 rounded-full flex items-center justify-center text-gray-500 dark:text-gray-300 shadow-md">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+        </svg>
+    </div>
+);
+
+export default UserIcon;
