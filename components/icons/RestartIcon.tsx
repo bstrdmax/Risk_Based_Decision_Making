73a8638c52c7c00725371c99bdc02efad89d7250
Jason Maxwell <max371979@gmail.com>
2025-11-07 09:03:42 -0500

    Create RestartIcon.tsx

---
+++ components/icons/RestartIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const RestartIcon: React.FC = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h5M20 20v-5h-5" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M4 9a9 9 0 0114.13-5.12M20 15a9 9 0 01-14.13 5.12" />
+    </svg>
+);
+
+export default RestartIcon;
