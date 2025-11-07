9053c361644e75a04258199d75aee9827dbee5b4
Jason Maxwell <max371979@gmail.com>
2025-11-07 09:03:37 -0500

    Create FeatureReportIcon.tsx

---
+++ components/icons/FeatureReportIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FeatureReportIcon: React.FC = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-full w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-1.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25A2.25 2.25 0 015.25 3h4.5M12 3v5.714a.75.75 0 00.53.707l5.26 1.879M12 3h4.5" />
+    </svg>
+);
+
+export default FeatureReportIcon;
