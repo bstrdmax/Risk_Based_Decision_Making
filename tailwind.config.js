b56cb769e257a0b6b22594ff9c1ba77dcafac95a
Jason Maxwell <max371979@gmail.com>
2025-11-07 08:38:45 -0500

    Update tailwind.config.js
    partxxxi

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,7 @@ import typography from '@tailwindcss/typography';
 export default {
   content: [
     "./index.html",
-    "./App.tsx",
-    "./index.tsx",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
